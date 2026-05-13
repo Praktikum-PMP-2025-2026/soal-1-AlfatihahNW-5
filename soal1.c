9d0a1d30c50d2e27a0926dfb3c8df4cd14de2077
AlfatihahNW <alfathfust44@gmail.com>
2026-05-13 10:42:24 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -28,76 +28,56 @@ long Combination(int n, int r) {
     }
 }
 
-void swap(char* left, char* right)
+
+
+void _printParenthesis(int pos, int n, int open, int close);
+
+void printParenthesis(int n)
 {
-    char temp = *left;
-    *left = *right;
-    *right = temp;
+    if (n > 0)
+        _printParenthesis(0, n, 0, 0);
+    return;
 }
-int compare (const void * a, const void * b)
+
+void _printParenthesis(int pos, int n, int open, int close)
 {
-  return ( *(char*)a - *(char*)b );
-}
-void PrintSortedPermutations(char* inStr)
-{
-    // Re-implementation of algorithm described here:
-    // http://www.geeksforgeeks.org/lexicographic-permutations-of-string/
-    int strSize = strlen(inStr);
-    // 0. Ensure input container is sorted
-    qsort(inStr, strSize, sizeof(char), compare);
-
-
-    int largerPermFound = 1;
-    do{
-        // 1. Print next permutation
-        printf("%s\n", inStr);
-        // 2. Find rightmost char that is smaller than char to its right
-        int i;
-        for (i = strSize - 2; i >= 0 && inStr[i] >= inStr[i+1]; --i){}
-
-        // if we couldn't find one, we're finished, else we can swap somewhere
-        if (i > -1)
-        {
-            // 3 find character at index j such that 
-            // inStr[j] = min(inStr[k]) && inStr[k] > inStr[i] for all k > i
-            int j = i+1;
-            int k;
-            for(k=j;k<strSize && inStr[k];++k)
-            {
-                if (inStr[k] > inStr[i] && inStr[k] < inStr[j])
-                    j = k;
-            }
-
-            // 3. Swap chars at i and j
-            swap(&inStr[i], &inStr[j]);
-
-            // 4. Sort string to the right of i
-            qsort(inStr+i+1, strSize-i-1, sizeof(char), compare);
+    static char str[100];
+ 
+    if (close == n) {
+        printf("%s \n", str);
+        return;
+    }
+    else {
+        if (open < n) {
+            str[pos] = '(';
+            _printParenthesis(pos + 1, n, open + 1, close);
         }
-        else
-        {
-            largerPermFound = 0;
+        if (open > close) {
+            str[pos] = ')';
+            _printParenthesis(pos + 1, n, open, close + 1);
         }
-    }while(largerPermFound);
+ 
+        
+    }
 }
 
 int main(){
     int n;
     char str[50];
     scanf("%d",&n);
-    if (n==4) 
-        strcpy(str,"(((())))");
-    else if(n==3)
-        strcpy(str,"((()))");
-    else if(n==2)
-        strcpy(str,"(())");
-    else if(n==1)
-        strcpy(str,"()");
-    else if(n==0)
-        strcpy(str,"");
+    // if (n==4) 
+    //     strcpy(str,"(((())))");
+    // else if(n==3)
+    //     strcpy(str,"((()))");
+    // else if(n==2)
+    //     strcpy(str,"(())");
+    // else if(n==1)
+    //     strcpy(str,"()");
+    // else if(n==0)
+    //     strcpy(str,"");
 
     
-    PrintSortedPermutations(str);
+    printParenthesis(n);
     int total = Combination(2*n,n)/(n+1);
     printf("TOTAL %d",total);
     return 0;
